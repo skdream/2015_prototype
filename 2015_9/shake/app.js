0d7583230443f6775f5295e3303aa1ae705b12f9
regexp-lin <regexp_lin@163.com>
2015-09-04 12:19:49 +0800

    local server

---
+++ 2015_9/shake/app.js
@@ -0,0 +1,17 @@
+var express = require("express"); 
+var app = express(); 
+var port = "80"; 
+var localhost = "192.168.1.104"; 
+// var localhost = "127.0.0.1";
+
+app.use(express.static('public'));
+
+app.get('/index', function(req, res) {
+	console.log('coming')
+	res.status(200).end('hi')
+})
+
+app.listen(port, localhost, function() {
+	console.log("listening on:" + localhost + ":" + port);
+});
+
